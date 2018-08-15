3a396a898cfc87be66afc2b1d7ae34abf735e0d1
Zlanquapea Troh <emmanueltroh@Emmanuels-MBP.hsd1.ma.comcast.net>
2018-08-15 17:43:23 -0400

    Added MY access tokens to the main.js

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibGlibWFubnkiLCJhIjoiY2prdTlob3E3MGI0azN3bnU1ZnB6azh2aiJ9.RVNSM_SH97FgBTIDJ9iK7A',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
