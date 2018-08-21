50b270a7175d5872d74505e166b04de330fffca3
Zlanquapea Troh <emmanueltroh@Emmanuels-MBP.hsd1.ma.comcast.net>
2018-08-21 18:13:04 -0400

    Added a event handler function to fetch caches and return requests

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,21 +1,21 @@
 let CACHE_NAME = 'restaurant_v1';
-let cachesUrl = [
-  'index.html', 'restaurant.html',
-  '/js/main.js', '/js/dbhelper.js',
-  '/js/restaurant_info.js', '/img',
-  '/css/styles.css'
-];
+// let cachesUrl = [
+//   'index.html', 'restaurant.html',
+//   '/js/main.js', '/js/dbhelper.js',
+//   '/js/restaurant_info.js', '/img',
+//   '/css/styles.css'
+// ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(cachesUrl);
-      }).catch(error => {
-        console.log('FAILED TO OPEN CACHE', error);
-      })
-  )
+  // e.waitUntil(
+  //   caches.open(CACHE_NAME)
+  //     .then(cache => {
+  //       console.log('Opened cache');
+  //       return cache.addAll(cachesUrl);
+  //     }).catch(error => {
+  //       console.log('FAILED TO OPEN CACHE', error);
+  //     })
+  // )
 });
 self.addEventListener('activate', e => {
   e.waitUntil(
@@ -32,3 +32,20 @@ self.addEventListener('activate', e => {
       })
   )
 })
+// calling the fetch event
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        //clone the respose
+        const requestClone = res.clone();
+        caches
+          .open(CACHE_NAME)
+          .then(cache => {
+            //add resopnse to cache
+            cache.put(e.request, requestClone);
+          });
+          return res;
+      }).catch( error => caches.match(e.request).then(res => res))
+  );
+});
